2dd4c9097c7fd22d2198e60527b2d7341980ec92
hyunsokstar <hyunsokstar@gmail.com>
2023-12-13 22:30:25 +0900

    체크 박스 체크 하면 배경색 토글

--- frontend-user-board/src/pages/users/ReactDataGrid.tsx
+++ frontend-user-board/src/pages/users/ReactDataGrid.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Center, Checkbox } from '@chakra-ui/react';
 import 'react-data-grid/lib/styles.css';
-import DataGrid from 'react-data-grid';
+import DataGrid, { Column, RenderCellProps, RenderHeaderCellProps, SelectCellFormatter, useRowSelection } from 'react-data-grid';
 import { apiForGetAllUsers } from '../api/apiForUserBoard';
 import { useQuery } from '@tanstack/react-query';
 
@@ -21,8 +21,57 @@ type UserBoardData = {
   perPage: number;
 };
 
+
+function HeaderRenderer(props: RenderHeaderCellProps<unknown>) {
+  const [isRowSelected, onRowSelectionChange] = useRowSelection();
+
+  return (
+    <SelectCellFormatter
+      aria-label="Select All"
+      tabIndex={props.tabIndex}
+      value={isRowSelected}
+      onChange={(checked) => {
+        onRowSelectionChange({ type: 'HEADER', checked });
+      }}
+    />
+  );
+}
+
+function SelectFormatter(props: RenderCellProps<unknown>) {
+  const [isRowSelected, onRowSelectionChange] = useRowSelection();
+
+  return (
+    <SelectCellFormatter
+      aria-label="Select"
+      tabIndex={props.tabIndex}
+      value={isRowSelected}
+      onChange={(checked, isShiftClick) => {
+        onRowSelectionChange({ type: 'ROW', row: props.row, checked, isShiftClick });
+      }}
+    />
+  );
+}
+
+export const SelectColumn: Column<any, any> = {
+  key: "select-row",
+  name: '',
+  width: 35,
+  minWidth: 35,
+  maxWidth: 35,
+  resizable: false,
+  sortable: false,
+  frozen: true,
+  renderHeaderCell(props) {
+    return <HeaderRenderer {...props} />;
+  },
+  renderCell(props) {
+    return <SelectFormatter {...props} />;
+  },
+};
+
 const columns = [
-  { key: 'id', name: 'ID' },
+  // { key: 'id', name: 'ID' },
+  SelectColumn,
   { key: 'email', name: 'Email' },
   { key: 'nickname', name: 'Nickname' },
   { key: 'role', name: 'Role' },
@@ -42,6 +91,7 @@ type Row = {
 const ReactDataGrid = () => {
   const [rows, setRows] = useState<Row[]>([]);
   const [pageNum, setPageNum] = useState(1);
+  const [selectedRows, setSelectedRows] = useState((): ReadonlySet<number> => new Set());
 
   const { isLoading, error, data: dataForUserBoard } = useQuery<UserBoardData>({
     queryKey: ['apiForGetAllUsers', pageNum],
@@ -71,7 +121,25 @@ const ReactDataGrid = () => {
 
   return (
     <Box width={'80%'} mx={'auto'} mt={5}>
-      <DataGrid columns={columns} rows={rows} rowKeyGetter={rowKeyGetter} />
+
+      <Box mb={4}>
+        <p>선택된 행의 ID:</p>
+        <ul>
+          {Array.from(selectedRows).map((id) => (
+            <li key={id}>{id}</li>
+          ))}
+        </ul>
+      </Box>
+
+      <DataGrid
+        columns={columns}
+        rows={rows}
+        rowKeyGetter={rowKeyGetter}
+        selectedRows={selectedRows}
+        onSelectedRowsChange={setSelectedRows}
+        onRowsChange={setRows}
+        className="fill-grid"
+      />
     </Box>
   );
 };
